debe1685b59f60e5b18b2948bce074a2f7c3f1c1
Mark Ni <xixian.ni@gmail.com>
2014-06-01 14:04:45 -0700

    refactor, using 1d array instead of 2d array

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,15 +3,13 @@
 angular.module('clashApp')
 	.controller('MainCtrl', function ($scope, Squad, Dead, Spear, Healer, Tank, Archer, $timeout) {
 
-
 		var snd = new Audio("sounds/clash.mp3"); // buffers automatically when created
 		var snd2 = new Audio("sounds/clash2.mp3");
 		var snd3 = new Audio("sounds/clash3.mp3");
 		var snd4 = new Audio("sounds/clash4.mp3");
-		var sfx = [snd,snd2,snd3,snd4];
+		var sfx = [snd, snd2, snd3, snd4];
 		var clickSfx = new Audio("sounds/click.mp3"); // buffers automatically when created
 		var gems = [];
-
 
 		var selected = null;
 		var targeted = null;
@@ -41,25 +39,20 @@ angular.module('clashApp')
 		var combo = 0;
 		var combos = [];
 
-
-
 		var highScore = parseInt(localStorage[localStorageKeyName] || 0);
 
 		$scope.enemyMatrix = [];
 
 		$scope.matrix = [];
 
-		$timeout(function(){
+		$timeout(function () {
 			gems = new Array($scope.MAXMOVESLEFT);
 			$scope.enemyMatrix = enemySquad.getMatrix();
 
 			$scope.matrix = squad.getMatrix();
-		},500);
+		}, 500);
 
 		var killed = 0;
-
-
-
 
 		$scope.resetGame = function () {
 			clickSfx.play();
@@ -87,8 +80,6 @@ angular.module('clashApp')
 
 		};
 
-
-
 		$scope.displayClass = {
 			selected: function (soldier) {
 				return soldier === selected ? 'selected' : '';
@@ -103,7 +94,7 @@ angular.module('clashApp')
 				if (dir === 'up') {
 					return 'bendup';
 				}
-				else if (dir === 'left'){
+				else if (dir === 'left') {
 					return 'hideleft';
 				}
 				else {
@@ -111,8 +102,8 @@ angular.module('clashApp')
 				}
 
 			},
-			deathAnimation:function(soldier){
-					return (soldier.health<=0 && soldier instanceof Dead !== true) ? 'killed': '';
+			deathAnimation: function (soldier) {
+				return (soldier.health <= 0 && soldier instanceof Dead !== true) ? 'killed' : '';
 			},
 			getGemClass: function (index) {
 
@@ -131,39 +122,46 @@ angular.module('clashApp')
 			inRanges = [];
 		};
 
-		$scope.showInRanges = function (isSelfMaxtrix, soldier) {
+		$scope.showInRanges = function (isSelfSquad, soldier) {
 
-
-			var matrix;
-			var enemyMatrix;
+			var _squad;
+			var _enemySquad;
 			var x = soldier.x;
 			var y = soldier.y;
 
-			if (isSelfMaxtrix) {
-				matrix = $scope.matrix;
-				enemyMatrix = $scope.enemyMatrix;
+			if (isSelfSquad) {
+				_squad = squad;
+				_enemySquad = enemySquad;
 
 			}
 			else {
-				matrix = $scope.enemyMatrix;
-				enemyMatrix = $scope.matrix;
+				_squad = enemySquad;
+				_enemySquad = squad;
 			}
 
 			if (soldier instanceof Healer) {
 
-				if (matrix[x - 1] && matrix[x - 1][y])    inRanges.push(matrix[x - 1][y]); // heal up;
-				if (matrix[x + 1] && matrix[x + 1][y])    inRanges.push(matrix[x + 1][y]); // heal down;
-				if (matrix[x] && matrix[x][y - 1])    inRanges.push(matrix[x][y - 1]); // heal left;
-				if (matrix[x] && matrix[x][y + 1])    inRanges.push(matrix[x][y + 1]); // heal right;
+				var soldierUp = _squad.getSoldierByPos(x - 1, y);
+				var soldierDown = _squad.getSoldierByPos(x + 1, y);
+				var soldierLeft = _squad.getSoldierByPos(x, y - 1);
+				var soldierRight = _squad.getSoldierByPos(x, y + 1);
+
+				if (soldierUp)    inRanges.push(soldierUp); // heal up;
+				if (soldierDown)    inRanges.push(soldierDown); // heal down;
+				if (soldierLeft)    inRanges.push(soldierLeft); // heal left;
+				if (soldierRight)    inRanges.push(soldierRight); // heal right;
 			}
+
 			else {
-				if (enemyMatrix[soldier.attackRange - 1 - x] && enemyMatrix[soldier.attackRange - 1 - x][y])
-					inRanges.push(enemyMatrix[soldier.attackRange - 1 - x][y]);
+				var targetSoldier = _enemySquad.getSoldierByPos(soldier.attackRange - 1 - x, y);
+				if (targetSoldier) {
+					inRanges.push(targetSoldier);
+				}
 
 			}
 
 			//display enemies beging targeted with highlight;
-		}
+		};
 
 		$scope.getAbsPos = function (reverse, soldier) {
 			var style = {};
@@ -201,43 +199,56 @@ angular.module('clashApp')
 
 		};
 
-		$scope.getScore = function(){
+		$scope.getScore = function () {
 			return score;
 		};
-		
-		$scope.getKilled = function(){
-			return killed;
 
+		$scope.getKilled = function () {
+			return killed;
 
 		};
 
-		$scope.getGems = function(){
+		$scope.getGems = function () {
 			return gems;
-		}
+		};
 
-		$scope.getCombos = function(){
-			if($scope.gameOver){
+		$scope.getCombos = function () {
+			if ($scope.gameOver) {
 				return combos;
 			}
-			else{
+			else {
 				return [];
 			}
 		};
-		
 
-		$scope.getHighScore = function(){
+		$scope.updateScore = function () {
+			score++;
+
+			combo++;
+			killed++;
+
+			$timeout(function () {
+				if (highScore < score) {
+					highScore = score;
+					localStorage[localStorageKeyName] = score;
+
+				}
+			})
+		};
+
+		$scope.getHighScore = function () {
 			return highScore;
 		};
 
-		$scope.playSound = function(){
-			if(sfx.length===0){
-				sfx =[snd,snd2,snd3,snd4];
+		$scope.playSound = function () {
+			if (sfx.length === 0) {
+				sfx = [snd, snd2, snd3, snd4];
 			}
 
-			var s = sfx.splice(Math.floor(Math.random()*sfx.length),1)[0];
+			var s = sfx.splice(Math.floor(Math.random() * sfx.length), 1)[0];
 			s.play();
 
-		} ;
+		};
 
 		$scope.clash = function () {
 
@@ -249,62 +260,74 @@ angular.module('clashApp')
 			$scope.runClashAnimation();
 
 			$timeout(function () {
-				heal();
-				battle();
+				heal(squad);
+				heal(enemySquad);
 
 				$scope.playSound();
+				battle();
 
 			}, 900);
 
 			$timeout(function () {
-				removeDeadBody();
-				removeDeadBody();
-				if(combo){
-					combos[combo] === undefined ? combos[combo] = 1 : combos[combo] ++;
+
+
+			}, 1200);
+
+			$timeout(function () {
+
+				removeDeadBody(squad, 1);
+				removeDeadBody(enemySquad, 0);
+				removeDeadBody(squad, 1);
+				removeDeadBody(enemySquad, 0);
+				refill(enemySquad);
+
+//				removeDeadBody(squad,1);
+//				removeDeadBody(enemySquad,0);
+
+				if (combo) {
+					combos[combo] === undefined ? combos[combo] = 1 : combos[combo]++;
 				}
-				score += parseInt(Math.pow(3.3,combo) -1);
+				score += parseInt(Math.pow(3.3, combo) - 1);
 				combo = 0;
 			}, 1500);
 
 			$scope.turn++;
 			$scope.movesLeft = 10;
 
-			function heal() {
-				var matrix = $scope.matrix;
-				var enemyMatrix = $scope.enemyMatrix;
-				for (var x = 0; x < HEIGHT; x++) {
-					for (var y = 0; y < WIDTH; y++) {
+			function heal(_squad) {
+				for (var x = 0; x < _squad.h; x++) {
+					for (var y = 0; y < _squad.w; y++) {
 
-						var mySoldier = matrix[x][y];
-						var enemySoldier = enemyMatrix[x][y];
-						if (mySoldier && mySoldier.className === "healer") {
+						var soldier = _squad.getSoldierByPos(x, y);
 
-							healAround(mySoldier, matrix);
-						}
-						if (enemySoldier && enemySoldier.className === "healer") {
-							healAround(enemySoldier, enemyMatrix);
+						if (soldier && soldier instanceof Healer) {
+
+							healAround(soldier, _squad);
 						}
 
 					}
 				}
 
 				function healAround(soldier, m) {
-					if (!soldier || soldier.className === 'dead') return;
+					if (!soldier || soldier instanceof Dead) return;
 					var x = soldier.x;
 					var y = soldier.y;
-					if (m[x - 1] && m[x - 1][y])    healBy(m[x - 1][y]); // heal up;
-					if (m[x + 1] && m[x + 1][y])    healBy(m[x + 1][y]); // heal down;
-					if (m[x] && m[x][y - 1])    healBy(m[x][y - 1]); // heal left;
-					if (m[x] && m[x][y + 1])    healBy(m[x][y + 1]); // heal right;
+					var soldierUp = _squad.getSoldierByPos(x - 1, y);
+					var soldierDown = _squad.getSoldierByPos(x + 1, y);
+					var soldierLeft = _squad.getSoldierByPos(x, y - 1);
+					var soldierRight = _squad.getSoldierByPos(x, y + 1);
+
+					if (soldierUp)    healBy(soldierUp); // heal up;
+					if (soldierDown)    healBy(soldierDown); // heal down;
+					if (soldierLeft)    healBy(soldierLeft); // heal left;
+					if (soldierRight)    healBy(soldierRight); // heal right;
 
 				}
 
 				function healBy(soldier) {
-					var currentH = soldier.health;
 					var nextH = soldier.health + 1;
 					soldier.health = Math.min(nextH, soldier.MAXHEALTH);
 					console.log('healed');
-
 				}
 
 			}
@@ -313,130 +336,181 @@ angular.module('clashApp')
 				var matrix = $scope.matrix;
 				var enemyMatrix = $scope.enemyMatrix;
 
-				function attack(soldier, isSelf){
-				 	if(!soldier || soldier instanceof Dead){
+				function attack(soldier, isSelf) {
+					if (!soldier || soldier instanceof Dead) {
 						return;
 					}
 
-					var em;
-					if (isSelf){
+					var _enemySquad;
+					if (isSelf) {
 
-						em = enemyMatrix;
+						_enemySquad = enemySquad;
 					}
-					else{
-						em = matrix;
+					else {
+						_enemySquad = squad;
 					}
 
-					var enemyXPos = soldier.attackRange - 1 - soldier.x;
+					var targetSoldier = _enemySquad.getSoldierByPos(soldier.attackRange - 1 - soldier.x, soldier.y);
 
-					if (em[enemyXPos]) {
+					if (targetSoldier && targetSoldier instanceof Dead !== true) {
 
-						var targetSoldier = em[enemyXPos][soldier.y];
-						if (targetSoldier && targetSoldier instanceof Dead !== true) {
-
-							targetSoldier.health -= soldier.attack;
-						}
+						targetSoldier.health -= soldier.attack;
 					}
+
 				}
 
 				for (var x = 0; x < 2; x++) {
 					for (var y = 0; y < WIDTH; y++) {
-						var mySoldier = matrix[x][y];
-						var enemySoldier = enemyMatrix[x][y];
-						attack(mySoldier,1);
-						attack(enemySoldier,0);
+						var mySoldier = squad.getSoldierByPos(x, y);
+						var enemySoldier = enemySquad.getSoldierByPos(x, y);
+						attack(mySoldier, 1);
+						attack(enemySoldier, 0);
 					}
 				}
 			}
 
-			function removeDeadBody() {
-				var matrix = $scope.matrix;
-				var enemyMatrix = $scope.enemyMatrix;
-				for (var x = 0; x < 2; x++) {
-					for (var y = 0; y < WIDTH; y++) {
-						check(matrix, x, y, squad);
-						check(enemyMatrix, x, y, enemySquad);
-
-					}
-				}
-
-				for (var x = 0; x < 2; x++) {
-					for (var y = 0; y < WIDTH; y++) {
-						shift(matrix, x, y, squad, false);
-						shift(enemyMatrix, x, y, enemySquad, true);
-
-					}
-				}
-
-
-
-				function check(m, x, y, s) {
-					var soldier = m[x][y];
-					if (soldier && soldier.health < 1 && soldier instanceof Dead !== true) {
-						console.log('fire arrow');
-						m[x][y] = undefined;
-
-					}
-					if (soldier instanceof Dead === true && m[x+1] && m[x+1][y] && m[x+1][y] instanceof Dead !== true) {
-						console.log('force to move forward');
-						m[x][y] = undefined;
-
-					}
-
-				}
-
-				function shift(m, x, y, s, t) {
-					var soldier = m[x][y];
-					if (!soldier) {
-						console.log('find dead body');
-						//find dead body
-						while (m[x + 1] && m[x + 1][y] && m[x + 1][y] instanceof Dead !== true) {
-							m[x][y] = m[x + 1][y];
-							m[x][y].x = x;
-							m[x][y].y = y;
-							x = x + 1;
+			function refill(_squad){
+				for (var x = 0; x < _squad.h; x++) {
+					for (var y = 0; y < _squad.w; y++) {
+						var soldier = _squad.getSoldierByPos(x, y);
+						if (soldier && soldier instanceof Dead){
+								var newSoldier = _squad.getANewSoldier();  //Pick a random solider
+								_squad.setSoldierByPos(x, y, newSoldier);
 						}
 
-						if (t) {
+					}
+				}
+			}
+
+			function removeDeadBody(_squad, isSelf) {
+				for (var x = 0; x < _squad.h; x++) {
+					for (var y = 0; y < _squad.w; y++) {
+						var soldier = _squad.getSoldierByPos(x, y);
+						var soldierBellow = _squad.getSoldierByPos(x + 1, y);
+
+						if (soldier && soldier.health < 1 && soldier instanceof Dead !== true) {
+
+							console.log('markdead');
+
+							if (!isSelf) {
+								//if dead found in enemy squad, update scores;
+
+								$scope.updateScore();
 
 
-
-							score++;
-
-							combo++;
-							killed++;
-
-							var Soldier = enemySquad.getANewSoldier();  //Pick a random solider
-							Soldier.x = x;
-							Soldier.y = y;
-							m[x][y] = Soldier;
-							$timeout(function(){
-								if (highScore <score ) {
-									highScore = score ;
-									localStorage[localStorageKeyName] = score ;
-
-
-								}
-							})
-
-						}
-						else {
-							m[x][y] = new Dead();
-							m[x][y].x = x;
-							m[x][y].y = y;
-
-							totalNumSoldiers--;
-
-							if (totalNumSoldiers <= 0) {
-								$scope.gameOver = true;
 
 							}
 
+							else{
+								totalNumSoldiers--;
+
+								if (totalNumSoldiers <= 0) {
+									$scope.gameOver = true;
+
+								}
+							}
+
+							var soldier = new Dead();
+							_squad.setSoldierByPos(x, y, soldier);
+
+
+
+						}
+						if (soldier && soldier instanceof Dead && soldierBellow && soldierBellow instanceof Dead !== true) {
+							console.log('swap');
+							_squad.swap(soldier, soldierBellow);
 						}
 
 					}
 				}
+
 			}
+
+//			function removeDeadBody(_squad) {
+//
+//				for (var x = 0; x < 2; x++) {
+//					for (var y = 0; y < _squad.w; y++) {
+//						check(x, y, _squad);
+//					}
+//				}
+//
+//				for (var x = 0; x < 2; x++) {
+//					for (var y = 0; y < _squad.w; y++) {
+//						shift(x, y, _squad, false);
+//					}
+//				}
+//
+//				function check(x, y, s) {
+//					var soldier = s.getSoldierByPos(x,y);
+//					var soldierBellow = s.getSoldierByPos(x+1,y);
+//					if (soldier && soldier.health < 1 && soldier instanceof Dead !== true) {
+//						s.setSoldierByPos(x,y,undefined);
+//
+//					}
+//
+//					//todo:what is this???
+//					if (soldier instanceof Dead === true && soldierBellow && soldierBellow instanceof Dead !== true) {
+//						console.log('force to move forward');
+//						s.setSoldierByPos(x,y,undefined);
+//
+//					}
+//
+//				}
+//
+//				//move up to fill this space
+//
+//				function shift(x, y, s, t) {
+//					var soldier = s.getSoldierByPos(x,y);
+//					if (!soldier) {
+//						console.log('find dead body');
+//						//find dead body
+//						var soldierBellow = s.getSoldierByPos(x+1,y);
+//
+//						while (soldierBellow && soldierBellow instanceof Dead !== true) {
+//							m[x][y] = m[x + 1][y];
+//							m[x][y].x = x;
+//							m[x][y].y = y;
+//							x = x + 1;
+//							soldierBellow = s.getSoldierByPos(x+1,y);
+//						}
+//
+//						if (t) {
+//
+//							score++;
+//
+//							combo++;
+//							killed++;
+//
+//							var Soldier = enemySquad.getANewSoldier();  //Pick a random solider
+//							Soldier.x = x;
+//							Soldier.y = y;
+//							m[x][y] = Soldier;
+//							$timeout(function () {
+//								if (highScore < score) {
+//									highScore = score;
+//									localStorage[localStorageKeyName] = score;
+//
+//								}
+//							})
+//
+//						}
+//						else {
+//							m[x][y] = new Dead();
+//							m[x][y].x = x;
+//							m[x][y].y = y;
+//
+//							totalNumSoldiers--;
+//
+//							if (totalNumSoldiers <= 0) {
+//								$scope.gameOver = true;
+//
+//							}
+//
+//						}
+//
+//					}
+//				}
+//			}
 
 		};
 
